45dcad04baa9284ce799bae8d5e4b45bad134f04
cheriaunt <alyonakravetsdenisovna@gmail.com>
2022-12-09 00:56:20 +0600

    add components

--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,22 @@
-import './app.css';
+import classes  from './app.module.scss';
+import Logo from './Logo.svg'
+
+// import Header from '../header';
+import Filter from '../filter'
+import TicketList from '../ticket-list';
+
+console.log(classes);
 
 function App() {
   return (
-    <div className="App">
+    <div className={classes.app}>
+      
+      <img className={classes.logotype} src={Logo} alt='logotype' />
+      
+      <main className={classes['app-main']}>
+      <Filter />
+      <TicketList />
+      </main>
     </div>
   );
 }
---
+++ src/ticket/ticket.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+import classes from './ticket.module.scss'
+
+
+const Ticket = () => {
+
+}
+
+export default Ticket;
